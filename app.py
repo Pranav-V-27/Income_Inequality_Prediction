85fc58b11585b5394b95508b40707b42169657b9
Pranav V <129186616+Pranav-V-27@users.noreply.github.com>
2023-10-12 19:15:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import streamlit as st
 import joblib
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 from prediction_template import get_prediction, label_encoder
 
 # Load the trained model
-model = ('Model/logistic_regression_model.pkl')
+model = joblib.load('Model/logistic_regression_model.pkl')
 
-def label_encoder(input_val, feats): 
-    feat_val = list(1+np.arange(len(feats)))
-    feat_key = feats
-    feat_dict = dict(zip(feat_key, feat_val))
-    value = feat_dict[input_val]
-    return value
-   
+# Load the label encoders for categorical features
+label_encoders = {
+    'education': joblib.load('Model/education_label_encoder.pkl'),
+    'marital_status': joblib.load('Model/marital_status_label_encoder.pkl'),
+    'occupation': joblib.load('Model/occupation_label_encoder.pkl'),
+    'race': joblib.load('Model/race_label_encoder.pkl'),
+    'sex': joblib.load('Model/sex_label_encoder.pkl')
+}
+
 # Create title and intro
 st.title('Income Inequality Prediction App')
 st.write("""
@@ -24,7 +25,6 @@ This app predicts whether a person's income will be above the limit based on cen
 st.subheader('User Input Parameters')
 
 age = st.number_input('Age', min_value=0, max_value=100, value=25)
-workclass = st.selectbox('Work Class', ['Private', 'Self-emp-not-inc', 'Self-emp-inc', 'Federal-gov', 'Local-gov', 'State-gov', 'Without-pay', 'Never-worked'])
 education = st.selectbox('Education', ['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc', '9th', '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'])
 education_num = st.number_input('Education Num', min_value=0, max_value=16, value=10)
 marital_status = st.selectbox('Marital Status', ['Married-civ-spouse', 'Divorced', 'Never-married', 'Separated', 'Widowed', 'Married-spouse-absent', 'Married-AF-spouse'])
@@ -36,27 +36,26 @@ capital_loss = st.number_input('Capital Loss', min_value=0)
 hours_per_week = st.number_input('Hours Per Week', min_value=0)
 #native_country = st.text_input('Native Country')
 
-# Apply LabelEncoder to categorical features
-#label_encoder = ('Model/label_encoder.pkl')
+# Create a dictionary with user input data
 user_data = {
-   'age': age,
-   'education': label_encoder.transform([education])[0],  # Corrected this line
-   'education_num': education_num,
-   'marital_status': label_encoder.transform([marital_status])[0],
-   'occupation': label_encoder.transform([occupation])[0],
-   'race': label_encoder.transform([race])[0],
-   'sex': label_encoder.transform([sex])[0],
-   'capital_gain': capital_gain,
-   'capital_loss': capital_loss,
-   'hours_per_week': hours_per_week,
-   #'native_country': native_country
+    'age': age,
+    'education': education,
+    'education_num': education_num,
+    'marital_status': marital_status,
+    'occupation': occupation,
+    'race': race,
+    'sex': sex,
+    'capital_gain': capital_gain,
+    'capital_loss': capital_loss,
+    'hours_per_week': hours_per_week,
+    #'native_country': native_country
 }
 
-# Transform into DataFrame
-features = pd.DataFrame(user_data, index=[0])
+# Convert user input data into a DataFrame
+user_data_df = pd.DataFrame(user_data, index=[0])
 
-# Make prediction
-prediction = model.predict(features)
+# Make prediction using the get_prediction function
+prediction = get_prediction(user_data_df, model, label_encoders)
 
 # Output prediction
 st.subheader('Prediction')
