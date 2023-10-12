8d9eace525caa5173e23ffdcaf74c4f2e9c00126
Pranav V <129186616+Pranav-V-27@users.noreply.github.com>
2023-10-12 11:41:08 +0530

    Update app_template.py

--- app_template.py
+++ app_template.py
@@ -1,62 +1,46 @@
+# Import necessary libraries 
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
 
-# Load the trained model
-model_filename = 'logistic_regression_model.pkl' 
-model = joblib.load(model_filename)
+# Add title to the app
+st.title('App Template')
 
-# Function to predict income above limit
-def predict_income(data):
-    prediction = model.predict(data)
-    return prediction
+# Add a brief introduction for the app
+st.write("""
+This is a template Streamlit app that can be used as a starting point for building your own apps.
+""")
 
-# Streamlit app
-def main():
-    st.title('Income Inequality Prediction App')
+# Input widgets 
+# Take user inputs using widgets like text boxes, sliders, radio buttons etc.
 
-    st.write("This app predicts whether an individual's income is above or below a certain threshold.")
+name = st.text_input('Name')
+age = st.slider('Age', 1, 100, 25)
+gender = st.radio('Gender', ['Male', 'Female'])
 
-    # Create input widgets for the features
-    pickup_hour = st.number_input('Pickup Hour', min_value=0, max_value=23)
-    day_of_week = st.selectbox('Day of the Week', ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
-    hour_of_accident = st.number_input('Hour of Accident', min_value=0, max_value=23)
-    accident_cause = st.text_input('Accident Cause')
-    num_vehicles_involved = st.number_input('Number of Vehicles Involved', min_value=1)
-    vehicle_type = st.text_input('Vehicle Type')
-    driver_age = st.number_input('Driver Age', min_value=0)
-    accident_area = st.text_input('Accident Area')
-    driving_experience = st.number_input('Driving Experience', min_value=0)
-    lanes = st.number_input('Lanes', min_value=1)
+# Processing
+# Use the input variables for any processing or calculations required
 
-    # Map day of the week to numeric values
-    day_mapping = {
-        'Monday': 1,
-        'Tuesday': 2,
-        'Wednesday': 3,
-        'Thursday': 4,
-        'Friday': 5,
-        'Saturday': 6,
-        'Sunday': 7
-    }
-    day_of_week = day_mapping.get(day_of_week, 0)
+if gender=='Male':
+    st.write('Hello Mr.', name)
+else:
+    st.write('Hello Ms.', name)
+    
+# Display outputs
+# Display the outputs in text, tables, charts, plots etc. 
 
-    # Create a feature vector from user inputs
-    input_data = np.array([pickup_hour, day_of_week, hour_of_accident, accident_cause, num_vehicles_involved, vehicle_type,
-                        driver_age, accident_area, driving_experience, lanes]).reshape(1, -1)
+st.write('Your age is:', age)
 
-    # Predict the income status
-    if st.button('Predict Income Status'):
-        prediction = predict_income(input_data)
-        if prediction[0] == 1:
-            st.write("Predicted Income Status: Above Limit")
-        else:
-            st.write("Predicted Income Status: Below Limit")
+# Visualizations
+# Add any charts or plots using pandas, matplotlib etc.
 
-    st.sidebar.markdown("### Project Information")
-    st.sidebar.write("This app is a part of the Income Inequality Prediction project.")
-    st.sidebar.write("For more details, visit the project's repository on GitHub.")
+data = {
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}
+df = pd.DataFrame(data)
+st.line_chart(df)
 
-if __name__ == "__main__":
+# Run the app   
+if __name__ == '__main__':
     main()
