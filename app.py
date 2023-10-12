938d3eca552a41e7cd27541f59fe48f937bc0d4d
Pranav V <129186616+Pranav-V-27@users.noreply.github.com>
2023-10-12 22:59:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,12 +21,12 @@ This app predicts whether a person's income will be above the limit based on cen
 st.subheader('User Input Parameters')
 
 age = st.number_input('Age', min_value=0, max_value=100, value=25)
-education = st.selectbox('Education', ['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc', '9th', '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'])
+#education = st.selectbox('Education', ['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc', '9th', '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'])
 education_num = st.number_input('Education Num', min_value=0, max_value=16, value=10)
-marital_status = st.selectbox('Marital Status', ['Married-civ-spouse', 'Divorced', 'Never-married', 'Separated', 'Widowed', 'Married-spouse-absent', 'Married-AF-spouse'])
-occupation = st.text_input('Occupation')
-race = st.selectbox('Race', ['White', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other', 'Black'])
-sex = st.selectbox('Sex', ['Female', 'Male'])
+#marital_status = st.selectbox('Marital Status', ['Married-civ-spouse', 'Divorced', 'Never-married', 'Separated', 'Widowed', 'Married-spouse-absent', 'Married-AF-spouse'])
+#occupation = st.text_input('Occupation')
+#race = st.selectbox('Race', ['White', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other', 'Black'])
+#sex = st.selectbox('Sex', ['Female', 'Male'])
 capital_gain = st.number_input('Capital Gain', min_value=0)
 capital_loss = st.number_input('Capital Loss', min_value=0)
 hours_per_week = st.number_input('Hours Per Week', min_value=0)
@@ -35,12 +35,12 @@ hours_per_week = st.number_input('Hours Per Week', min_value=0)
 # Create a dictionary with user input data
 user_data = {
    'age': age,
-   'education': label_encoder.transform([education])['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc', '9th', '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'],  # Corrected this line
+   #'education': label_encoder.transform([education])['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc', '9th', '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'],  # Corrected this line
    'education_num': education_num,
-   'marital_status': label_encoder.transform([marital_status])[0],
-   'occupation': label_encoder.transform([occupation])[0],
-   'race': label_encoder.transform([race])[0],
-   'sex': label_encoder.transform([sex])[0],
+   #'marital_status': label_encoder.transform([marital_status])[0],
+   #'occupation': label_encoder.transform([occupation])[0],
+   #'race': label_encoder.transform([race])[0],
+   #'sex': label_encoder.transform([sex])[0],
    'capital_gain': capital_gain,
    'capital_loss': capital_loss,
    'hours_per_week': hours_per_week,
