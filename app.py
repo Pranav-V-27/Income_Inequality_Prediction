8cf77a95c70f554af4f95e4ab870124fcd734419
Pranav V <129186616+Pranav-V-27@users.noreply.github.com>
2023-10-12 13:07:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,35 +15,34 @@ This app predicts whether a person's income will be above the limit based on cen
 st.subheader('User Input Parameters')
 
 age = st.number_input('Age', min_value=0, max_value=100, value=25)
-workclass = st.selectbox('Work Class', ['Private', 'Self-emp-not-inc', 'Self-emp-inc', 'Federal-gov', 'Local-gov', 'State-gov', 'Without-pay', 'Never-worked'])
-fnlwgt = st.number_input('FNLWGT', min_value=0) 
+#class = st.selectbox('Work Class', ['Private', 'Self-emp-not-inc', 'Self-emp-inc', 'Federal-gov', 'Local-gov', 'State-gov', 'Without-pay', 'Never-worked'])
+#fnlwgt = st.number_input('FNLWGT', min_value=0) 
 education = st.selectbox('Education', ['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc', '9th', '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'])
-education_num = st.number_input('Education Num', min_value=0, max_value=16, value=10)
+#education_num = st.number_input('Education Num', min_value=0, max_value=16, value=10)
 marital_status = st.selectbox('Marital Status', ['Married-civ-spouse', 'Divorced', 'Never-married', 'Separated', 'Widowed', 'Married-spouse-absent', 'Married-AF-spouse'])
 # occupation = st.text_input('Occupation')
-relationship = st.selectbox('Relationship', ['Wife', 'Own-child', 'Husband', 'Not-in-family', 'Other-relative', 'Unmarried'])  
+#relationship = st.selectbox('Relationship', ['Wife', 'Own-child', 'Husband', 'Not-in-family', 'Other-relative', 'Unmarried'])  
 race = st.selectbox('Race', ['White', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other', 'Black'])
-sex = st.selectbox('Sex', ['Female', 'Male'])
-capital_gain = st.number_input('Capital Gain', min_value=0)
-capital_loss = st.number_input('Capital Loss', min_value=0) 
-hours_per_week = st.number_input('Hours Per Week', min_value=0)
-native_country = st.text_input('Native Country')
+gender = st.selectbox('Gender', ['Female', 'Male'])
+gains = st.number_input('Capital Gain', min_value=0)
+losses = st.number_input('Capital Loss', min_value=0) 
+#hours_per_week = st.number_input('Hours Per Week', min_value=0)
+citizenship = st.text_input('citizenship')
 
 # Create feature dictionary    
 user_data = {
     'age': age,
-    'workclass': workclass,
-    'fnlwgt': fnlwgt,
+    #'workclass': class,
     'education': education,
-    'education_num': education_num,
+    #'education_num': education_num,
     'marital_status': marital_status,
-    'relationship': relationship,
+    #'relationship': relationship,
     'race': race,
-    'sex': sex,
-    'capital_gain': capital_gain,
-    'capital_loss': capital_loss,
-    'hours_per_week': hours_per_week,
-    'native_country': native_country
+    'sex': gender,
+    'capital_gain': gains,
+    'capital_loss': losses,
+    #'hours_per_week': hours_per_week,
+    'citizenship': citizenship
 }
 
 # Transform into DataFrame
