5b6b9286c7449f711108959658d6b79b927321a9
Pranav V <129186616+Pranav-V-27@users.noreply.github.com>
2023-10-12 19:14:47 +0530

    Update prediction_template.py

--- prediction_template.py
+++ prediction_template.py
@@ -1,18 +1,18 @@
 import joblib
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import LabelEncoder 
-
 from sklearn.preprocessing import LabelEncoder
 
 def label_encoder(input_val, encoder):
     encoded_val = encoder.transform([input_val])[0]
     return encoded_val
 
-
-
-def get_prediction(data,model):
+def get_prediction(data, model, label_encoders):
     """
     Predict the class of a given data point.
     """
+    # Apply label encoding to the data
+    for feature in label_encoders:
+        data[feature] = label_encoder(data[feature], label_encoders[feature])
+    
     return model.predict(data)
