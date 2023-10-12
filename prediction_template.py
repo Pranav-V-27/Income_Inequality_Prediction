ee19f0590bbb94c31f905ccbfe3019a922d35646
Pranav V <129186616+Pranav-V-27@users.noreply.github.com>
2023-10-12 11:55:24 +0530

    Update prediction_template.py

--- prediction_template.py
+++ prediction_template.py
@@ -3,7 +3,7 @@ import joblib
 import pandas as pd
 
 # Load the trained model
-model = joblib.load('classification_models.pkl') 
+model = joblib.load('logistic_regression_model.pkl') 
 
 # Create title and intro
 st.title('Income Inequality Prediction App')
