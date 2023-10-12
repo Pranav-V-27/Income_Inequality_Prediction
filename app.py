2468e4350c78e680a6b375adda476154bbb88cdb
Pranav V <129186616+Pranav-V-27@users.noreply.github.com>
2023-10-12 19:10:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ This app predicts whether a person's income will be above the limit based on cen
 st.subheader('User Input Parameters')
 
 age = st.number_input('Age', min_value=0, max_value=100, value=25)
-#workclass = st.selectbox('Work Class', ['Private', 'Self-emp-not-inc', 'Self-emp-inc', 'Federal-gov', 'Local-gov', 'State-gov', 'Without-pay', 'Never-worked'])
-#education = st.selectbox('Education', ['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc', '9th', '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'])
+workclass = st.selectbox('Work Class', ['Private', 'Self-emp-not-inc', 'Self-emp-inc', 'Federal-gov', 'Local-gov', 'State-gov', 'Without-pay', 'Never-worked'])
+education = st.selectbox('Education', ['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc', '9th', '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'])
 education_num = st.number_input('Education Num', min_value=0, max_value=16, value=10)
 marital_status = st.selectbox('Marital Status', ['Married-civ-spouse', 'Divorced', 'Never-married', 'Separated', 'Widowed', 'Married-spouse-absent', 'Married-AF-spouse'])
 occupation = st.text_input('Occupation')
@@ -40,16 +40,16 @@ hours_per_week = st.number_input('Hours Per Week', min_value=0)
 #label_encoder = ('Model/label_encoder.pkl')
 user_data = {
    'age': age,
-   #'education': label_encoder.transform([education])[0],  # Corrected this line
-    'education_num': education_num,
-    'marital_status': label_encoder.transform([marital_status])[0],
-    'occupation': label_encoder.transform([occupation])[0],
-    'race': label_encoder.transform([race])[0],
-    'sex': label_encoder.transform([sex])[0],
-    'capital_gain': capital_gain,
-    'capital_loss': capital_loss,
-    'hours_per_week': hours_per_week,
-    #'native_country': native_country
+   'education': label_encoder.transform([education])[0],  # Corrected this line
+   'education_num': education_num,
+   'marital_status': label_encoder.transform([marital_status])[0],
+   'occupation': label_encoder.transform([occupation])[0],
+   'race': label_encoder.transform([race])[0],
+   'sex': label_encoder.transform([sex])[0],
+   'capital_gain': capital_gain,
+   'capital_loss': capital_loss,
+   'hours_per_week': hours_per_week,
+   #'native_country': native_country
 }
 
 # Transform into DataFrame
