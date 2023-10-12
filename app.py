4b0a985a30235ebb6d59a918b848bf4bb99cfe5b
Pranav V <129186616+Pranav-V-27@users.noreply.github.com>
2023-10-13 00:00:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,65 @@
 import streamlit as st
-import joblib
 import pandas as pd
+import joblib
 
-from prediction_template import get_prediction, label_encoder
+# Load model
+model = joblib.load('logistic_regression_model.pkl')
 
-# Load the trained model
-try:
-    model = 'Model/logistic_regression_model.pkl'
-except Exception as e:
-    st.error(f"Error loading the model: {str(e)}")
+# Load label encoder 
+encoder = joblib.load('label_encoder.pkl')
 
-# Load the label encoders for categorical features
+st.title('Income Inequality Prediction')
 
-# Create title and intro
-st.title('Income Inequality Prediction App')
-st.write("""
-This app predicts whether a person's income will be above the limit based on census data.
-""")
+st.sidebar.header('User Input Parameters')
 
-# Get user input
-st.subheader('User Input Parameters')
+def user_input_features():
+    age = st.sidebar.slider('Age', 0, 100, 25)
+    workclass = st.sidebar.selectbox('Work Class', ['Private', 'Self-emp-not-inc', 'Self-emp-inc', 'Federal-gov', 'Local-gov', 'State-gov', 'Without-pay', 'Never-worked'])
+    fnlwgt = st.sidebar.slider('FNLWGT', min_value=0)
+    education = st.sidebar.selectbox('Education', ['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc', '9th', '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'])
+    education_num = st.sidebar.slider('Education Num', 0,16, 10)
+    marital_status = st.sidebar.selectbox('Marital Status', ['Married-civ-spouse', 'Divorced', 'Never-married', 'Separated', 'Widowed', 'Married-spouse-absent', 'Married-AF-spouse'])
+    occupation = st.sidebar.text_input('Occupation')
+    relationship = st.sidebar.selectbox('Relationship', ['Wife', 'Own-child', 'Husband', 'Not-in-family', 'Other-relative', 'Unmarried'])
+    race = st.sidebar.selectbox('Race', ['White', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other', 'Black'])
+    sex = st.sidebar.selectbox('Sex', ['Female', 'Male'])
+    capital_gain = st.sidebar.slider('Capital Gain', min_value=0)
+    capital_loss = st.sidebar.slider('Capital Loss', min_value=0)
+    hours_per_week = st.sidebar.slider('Hours Per Week', min_value=0)
+    native_country = st.sidebar.text_input('Native Country')
+    
+    data = {'age': age,
+            'workclass': workclass,
+            'fnlwgt': fnlwgt,
+            'education': education,
+            'education_num': education_num,
+            'marital_status': marital_status,
+            'occupation': occupation,
+            'relationship': relationship,
+            'race': race,
+            'sex': sex,
+            'capital_gain': capital_gain,
+            'capital_loss': capital_loss,
+            'hours_per_week': hours_per_week,
+            'native_country': native_country}
+    features = pd.DataFrame(data, index=[0])
+    return features
 
-age = st.number_input('Age', min_value=0, max_value=100, value=25)
-#education = st.selectbox('Education', ['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc', '9th', '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'])
-education_num = st.number_input('Education Num', min_value=0, max_value=16, value=10)
-#marital_status = st.selectbox('Marital Status', ['Married-civ-spouse', 'Divorced', 'Never-married', 'Separated', 'Widowed', 'Married-spouse-absent', 'Married-AF-spouse'])
-#occupation = st.text_input('Occupation')
-#race = st.selectbox('Race', ['White', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other', 'Black'])
-#sex = st.selectbox('Sex', ['Female', 'Male'])
-#capital_gain = st.number_input('Capital Gain', min_value=0)
-#capital_loss = st.number_input('Capital Loss', min_value=0)
-#hours_per_week = st.number_input('Hours Per Week', min_value=0)
-#native_country = st.text_input('Native Country')
+df = user_input_features()
 
-# Create a dictionary with user input data
-user_data = {
-   'age': age,
-   #'education': label_encoder.transform([education])['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc', '9th', '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'],  # Corrected this line
-   'education_num': education_num,
-   #'marital_status': label_encoder.transform([marital_status])[0],
-   #'occupation': label_encoder.transform([occupation])[0],
-   #'race': label_encoder.transform([race])[0],
-   #'sex': label_encoder.transform([sex])[0],
-   #'capital_gain': capital_gain,
-   #'capital_loss': capital_loss,
-   #'hours_per_week': hours_per_week,
-   #'native_country': native_country
-}
+# Encoding categorical data
+for col in ['workclass', 'education', 'marital_status', 'occupation', 'relationship', 'race', 'sex', 'native_country']:
+    df[col] = encoder.transform(df[col]) 
 
+# Predict    
+prediction = model.predict(df)
 
-# Transform into DataFrame
-features = pd.DataFrame(user_data, index=[0])
-
-# Make prediction
-prediction = model.predict(features)
-
-# Output prediction
-st.subheader('Prediction')
-if prediction[0] == 1:
-    st.write('Income will likely be above the limit')
-else:
-    st.write('Income will likely not be above the limit')
+# Display
+if st.button('Predict'):
+    if prediction == 1:
+        st.subheader('Income will likely be above the limit')
+    else:
+        st.subheader('Income will likely not be above the limit')
+        
+st.subheader('Input Parameters')
+st.write(df)
